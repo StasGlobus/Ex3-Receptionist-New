c00bd7b0600424a37ee6e22f6006b394bc6581ae
Stas Globus <stani246@gmail.com>
2019-12-21 23:18:50 +0200

    that is for the day

--- Ex3/receptionist.c
+++ Ex3/receptionist.c
@@ -1,123 +1,63 @@
-// ProducerConsumerDemonstration.c
 
-/*
-* This is a c implementation for the problem discussed in the lecture.
-* We have a producer thread, which creates messages, and a consumer thread,
-* which reads them.
-*/
-
-
-/*
-// Pseudo-code of the bottom example:
-// ---------------------------------------
-
-const int N = 100;
-typedef int semaphore;
-semaphore mutex = 1;
-semaphore empty = N;
-semaphore full = 0;
-
-void producer()
-{
-	int item;
-	while(true)
-	{
-		item = produce_item();
-		down(&empty);
-		down(&mutex);
-		insert_item(item);
-		up(&mutex);
-		up(&full);
-	}
-}
-
- void consumer()
- {
-	 int item;
-	 while (true)
-	 {
-		 down(&full);
-		 down(&mutex);
-		 item = remove_item();
-		 up(&mutex);
-		 up(&empty);
-		 consume_item(item);
-	 }
-}
-*/
 
 // Includes --------------------------------------------------------------------
 
-#include "windows.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "receptionist.h"
 
-// Constants  ------------------------------------------------------------------
 
-#define NUM_OF_THREADS 2
-static const int TIMEOUT_IN_MILLISECONDS = 10 * 1000;
-static const int PRODUCER_WAIT_TIME_IN_MILISECONDS = 250;
-static const int CONSUMER_WAIT_TIME_IN_MILISECONDS = 500; /* Unequal wait times make the demonstration clearer */
-static const int N = 10;
 
-// Variables -------------------------------------------------------------------
 
-static HANDLE empty;
-static HANDLE full;
-static HANDLE mutex;
-static int items = 20;
-
-// Function Declarations -------------------------------------------------------
-
-static void ProducerConsumerUsingSemaphores();
-static DWORD Producer(LPVOID Dummy);
-static DWORD Consumer(LPVOID Dummy);
-static void ReportErrorAndEndProgram();
-static int produce_item();
-static void insert_item(int item);
-static int remove_item();
-static void consume_item(int item);
 
 // Function Definitions --------------------------------------------------------
 
-int main()
+void* Room_default(Room *room) {
+
+	room->name = (char*)malloc(MAX_STRING * sizeof(char));
+	room->price = (int*)clloc(sizeof(int));
+	room->price = (int*)clloc(sizeof(int));
+	room->size = (int*)calloc(1, sizeof(int));
+
+} 
+
+static DWORD ChkRoomStatus(LPVOID lpParam)
 {
-	ProducerConsumerUsingSemaphores();
+
+
+	return 0;
 }
 
-static void ProducerConsumerUsingSemaphores()
+int main()
 {
+	reception();
+}
+
+static void reception()
+{
+	
 	BOOL an_error_occured = FALSE;
-	int   p_action_list[] = { 0, 1, 1, 1, 1, 1, 0, 0, 1, 1 };
-	LPTHREAD_START_ROUTINE p_thread_routines[] =
-	{ (LPTHREAD_START_ROUTINE)Consumer,(LPTHREAD_START_ROUTINE)Producer };
+	//int   p_action_list[] = { 0, 1, 1, 1, 1, 1, 0, 0, 1, 1 };
+	LPTHREAD_START_ROUTINE p_thread_routine = (LPTHREAD_START_ROUTINE)ChkRoomStatus;
 	HANDLE p_thread_handles[NUM_OF_THREADS];
 	int i = 0;
 	int   p_thread_indices[] = { 1,2,3,4,5,6,7,8,9,10 };
 	DWORD thread_id;
+	Room *room;
 
-	empty = CreateSemaphore(
+	//Integration code -------------------------------------------------------
+	room = (Room*)malloc(sizeof(Room));// multiply by the amount of ID's in the filr
+	Room_default(room);
+	strcpy(room->name, "Twin-Room");
+	room->price = 10;
+	room->size = 2;
+	//------------------------------------------------------------------------
+	room_sem = CreateSemaphore(
 		NULL,	/* Default security attributes */
-		N,		/* Initial Count - all slots are empty */
+		N,		/* Initial Count - all slots are room_sem */
 		N,		/* Maximum Count */
 		NULL); /* un-named */
 
-	if (empty == NULL) { an_error_occured = TRUE; goto Main_Cleanup_0; }
+	if (room_sem == NULL) { an_error_occured = TRUE; goto Main_Cleanup_0; }
 
-	full = CreateSemaphore(
-		NULL,	/* Default security attributes */
-		0,		/* Initial Count - no slots are full */
-		N,		/* Maximum Count */
-		NULL); /* un-named */
-
-	if (full == NULL) { an_error_occured = TRUE; goto Main_Cleanup_1; }
-
-	mutex = CreateMutex(
-		NULL,   /* default security attributes */
-		FALSE,	/* don't lock mutex immediately */
-		NULL); /* un-named */
-
-	if (mutex == NULL) { an_error_occured = TRUE; goto Main_Cleanup_2; }
 
 
 	/* create the threads */
@@ -125,7 +65,7 @@ static void ProducerConsumerUsingSemaphores()
 		p_thread_handles[i] = CreateThread(
 			NULL,                                /* default security attributes */
 			0,                                   /* use default stack size */
-			p_thread_routines[p_action_list[i]], /* thread function */
+			p_thread_routine, /* thread function */
 			&p_thread_indices[i],                /* argument to thread function  */
 			0,                                   /* use default creation flags */
 			&thread_id                           /* returns the thread identifier  */
@@ -150,93 +90,17 @@ Main_Cleanup_3:
 	for (i = 0; i < NUM_OF_THREADS; i++) {
 		CloseHandle(p_thread_handles[i]);
 	}
-	CloseHandle(mutex);
-Main_Cleanup_2:
-	CloseHandle(full);
 Main_Cleanup_1:
-	CloseHandle(empty);
+	CloseHandle(room_sem);
 Main_Cleanup_0:
 	if (an_error_occured) ReportErrorAndEndProgram();
 }
 
-static DWORD Producer(LPVOID Dummy)
-{
-	DWORD wait_res;
-	BOOL release_res;
-	LONG previous_count;
 
-	while (TRUE)
-	{
-		/* Sleep for the purposes of the demonstration */
-		Sleep(PRODUCER_WAIT_TIME_IN_MILISECONDS);
 
-		produce_item();
 
-		wait_res = WaitForSingleObject(empty, INFINITE);
-		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
 
-		wait_res = WaitForSingleObject(mutex, INFINITE);
-		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
 
-		/* Start Critical Section */
-
-		insert_item(items);
-
-		/* End Critical Section */
-
-		release_res = ReleaseMutex(mutex);
-		if (release_res == FALSE) ReportErrorAndEndProgram();
-
-		release_res = ReleaseSemaphore(
-			full,
-			1, 		/* Signal that exactly one cell was filled */
-			&previous_count);
-		if (release_res == FALSE) ReportErrorAndEndProgram();
-		printf("Producer inserted one item. Previous count is: %ld\n", previous_count);
-	}
-
-	return 0;
-}
-
-static DWORD Consumer(LPVOID Dummy)
-{
-	DWORD wait_res;
-	BOOL release_res;
-	LONG previous_count;
-
-	int item;
-	while (TRUE)
-	{
-		/* Sleep for the purposes of the demonstration */
-		Sleep(CONSUMER_WAIT_TIME_IN_MILISECONDS);
-
-		wait_res = WaitForSingleObject(full, INFINITE);
-		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
-
-		wait_res = WaitForSingleObject(mutex, INFINITE);
-		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram();
-
-		/* Start Critical Section */
-
-		item = remove_item(items);
-
-		/* End Critical Section */
-
-		release_res = ReleaseMutex(mutex);
-		if (release_res == FALSE) ReportErrorAndEndProgram();
-
-		release_res = ReleaseSemaphore(
-			empty,
-			1, 		/* Signal that exactly one cell was emptied */
-			&previous_count);
-		if (release_res == FALSE) ReportErrorAndEndProgram();
-
-		consume_item(item);
-		printf("Consumer used one item. Previous count is: %ld\n", previous_count);
-	}
-
-	return 0;
-}
 
 static void ReportErrorAndEndProgram()
 {
--- Ex3/receptionist.h
+++ Ex3/receptionist.h
@@ -1,1 +1,44 @@
-#pragma once
+#ifndef __RECEPTIONIST_H__
+#define __RECEPTIONIST_H__
+
+// Includes --------------------------------------------------------------------
+#include "windows.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+//structs ----------------------------------------------------------------------
+typedef struct {
+
+	char  *name; // name of the room
+	int price; // price for single person for single night
+	int size; // The amount of pepople the room can hold at the same time
+	
+}Room; 
+// Constants  ------------------------------------------------------------------
+
+#define NUM_OF_THREADS 3
+#define MAX_STRING 100
+static const int TIMEOUT_IN_MILLISECONDS = 10 * 1000;
+static const int PRODUCER_WAIT_TIME_IN_MILISECONDS = 250;
+static const int CONSUMER_WAIT_TIME_IN_MILISECONDS = 500; /* Unequal wait times make the demonstration clearer */
+static const int N = 2; // integration number of places in one room_sem
+
+// Variables -------------------------------------------------------------------
+
+static HANDLE room_sem; //semaphoe
+//static HANDLE full;//semaphore
+//static HANDLE mutex;
+static int items = 20;
+
+// Function Declarations -------------------------------------------------------
+
+static void reception();
+static DWORD Producer(LPVOID Dummy);
+static void ReportErrorAndEndProgram();
+static int produce_item();
+static void insert_item(int item);
+static int remove_item();
+static void consume_item(int item);
+
+
+#endif //__RECEPTIONIST_H__
