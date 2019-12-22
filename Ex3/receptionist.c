22070aff716aff5b853d3ddc75e8bc9b66c4b354
Stas Globus <stani246@gmail.com>
2019-12-22 22:39:58 +0200

    end of day

--- Ex3/receptionist.c
+++ Ex3/receptionist.c
@@ -5,9 +5,6 @@
 #include "receptionist.h"
 
 
-
-
-
 // Function Definitions --------------------------------------------------------
 
 void* Room_default(Room *room) {
@@ -22,6 +19,9 @@ void* Guest_default(Guest *guest) {
 
 	guest->name = (char*)malloc(MAX_STRING * sizeof(char));
 	guest->money = (int*)calloc(1, sizeof(int));
+	guest->checked_in = (bool*)malloc(1, sizeof(bool));
+	guest->designated_room = (Room*)malloc(sizeof(Room));
+
 
 }
 void* Hotel_default(Hotel *hotel) {
@@ -35,42 +35,49 @@ static DWORD ChkRoomStatus(LPVOID lpParam)
 	DWORD wait_res;
 	BOOL release_res;
 	LONG previous_count;
-	while (TRUE)
-	{
+	Guest *guest = lpParam;
+	
+	
 		/* Sleep for the purposes of the demonstration */
 		Sleep(PRODUCER_WAIT_TIME_IN_MILISECONDS);
 
 		wait_res = WaitForSingleObject(room_sem, INFINITE);
 		if (wait_res != WAIT_OBJECT_0) ReportErrorAndEndProgram(); // if succeds, the semphore then room has space
-		printf("%s\n",wait_res);
-		
-		//produce_item(); // need to change it to a realfumction
-
-		/* Start Critical Section */
-
-		//insert_item(items);// 
-
-		/* End Critical Section */
-		
-	
-
-		release_res = ReleaseSemaphore(
-			room_sem,
-			1, 		/* Signal that exactly one cell was emptied */
-			&previous_count);
-		if (release_res == FALSE) ReportErrorAndEndProgram();
-
+		//printf("%s\n",wait_res);
+		while(TRUE) 
+		{
+			check_out(guest);
+			if (guest->money)
+				check_in(guest);
+			else
+			{
+				release_res = ReleaseSemaphore(
+					room_sem,
+					1, 		/* Signal that exactly one cell was emptied */
+					&previous_count);
+				if (release_res == FALSE) ReportErrorAndEndProgram();
+				break;
+			}
+			
+		}
 		//consume_item(item);
-		printf("Consumer used one item. Previous count is: %ld\n", previous_count);
+	//	printf("Consumer used one item. Previous count is: %ld\n", previous_count);
 
-	}
+	
 
 	return 0;
 }
 
 int main()
 {
-	reception();
+	Room *rooms=NULL;
+	char path[MAX_STRING] = { '\0' };
+	rooms = (Room*)malloc(sizeof(Room));
+	strcpy(path,"C:\\Users\\stani\\OneDrive\\Documents\\Limudim\\OS");
+	init_rooms(path, rooms);
+	printf("%s\n", rooms->name);
+	//reception();
+	getchar(); // stops the cmd window from closing
 }
 
 static void reception()
@@ -81,7 +88,7 @@ static void reception()
 	LPTHREAD_START_ROUTINE p_thread_routine = (LPTHREAD_START_ROUTINE)ChkRoomStatus;
 	HANDLE p_thread_handles[NUM_OF_THREADS];
 	int i = 0;
-	int   p_thread_indices[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int   p_thread_indices[] = { 1,2,3,4,5,6,7,8,9,10 };
 	DWORD thread_id;
 	Room *room;
 	Guest *guest;
@@ -98,17 +105,20 @@ static void reception()
 	Guest_default(guest);
 	strcpy(guest->name, "John Doe");
 	guest->money = 50;
+	guest->checked_in = FALSE;
+	guest->designated_room = room;
+	guest->day = 1;
 
 	hotel = (Hotel*)malloc(sizeof(Hotel));
-
-
-
+	Hotel_default(hotel);
+	hotel->room_ptr = room;
+	hotel->guest_ptr = guest;
 
 	//------------------------------------------------------------------------
 	room_sem = CreateSemaphore(
 		NULL,	/* Default security attributes */
-		N,		/* Initial Count - all slots are room_sem currently 2 */
-		N,		/* Maximum Count */
+		N,		/* Initial Count - all slots are room_sem currently 2 should be set according to room size */
+		N,		/* Maximum Count should be set according to room size*/
 		NULL); /* un-named */
 
 	if (room_sem == NULL) { an_error_occured = TRUE; goto Main_Cleanup_0; }
@@ -116,18 +126,18 @@ static void reception()
 
 
 	/* create the threads */
-	for (i = 0; i < NUM_OF_THREADS; i++) {
+	for (i = 0; i < NUM_OF_THREADS; i++) { // num of threads shoud be num of guests?
 		p_thread_handles[i] = CreateThread(
 			NULL,                                /* default security attributes */
 			0,                                   /* use default stack size */
 			p_thread_routine, /* thread function */
-			&p_thread_indices[i],                /* argument to thread function  */
+			guest,//&p_thread_indices[i],                /* argument to thread function  */
 			0,                                   /* use default creation flags */
 			&thread_id                           /* returns the thread identifier  */
 		);
 
 		if (p_thread_handles[i] == NULL) {
-			printf("CreateThread %d failed.\n", p_thread_indices[i]);
+			printf("CreateThread %s failed.\n", hotel->guest_ptr->name);
 			an_error_occured = TRUE; goto Main_Cleanup_3;
 		}
 	}
@@ -163,24 +173,70 @@ static void ReportErrorAndEndProgram()
 	exit(1);
 }
 
-static int produce_item()
+
+
+static void check_in(Guest *guest)
 {
-	/* dummy implementation, supplied just to make the program compile */
-	return 0;
+	if (!(guest->checked_in))
+	{
+		guest->checked_in = TRUE;
+		printf("\n%s %s IN %d\n", guest->designated_room->name, guest->name, guest->day);
+	}
+	guest->money = (guest->money) - (guest->designated_room->price);	
+	(guest->day)++;
 }
 
-static void check_in(int item)
+static void check_out(Guest *guest)
 {
-	/* dummy implementation, supplied just to make the program compile */
+	if (guest->checked_in)
+	{
+		if (!(guest->money))
+			printf("\n%s %s OUT %d\n", guest->designated_room->name, guest->name, guest->day);
+	}
 }
 
-static void check_out()
+
+static void init_rooms(char path[], Room *rooms) // create a semaphore for each room
 {
-	/* dummy implementation, supplied just to make the program compile */
-	return 0;
+	//Room *rooms;
+	FILE *ptr;
+	int i = 0;
+	char adress[MAX_STRING] = { '\0' };
+	//rooms = (Room*)malloc(sizeof(Room));// multiply by the amount of ID's in the filr
+	Room_default(rooms);
+	memcpy(adress, path, MAX_STRING);
+	strcat(adress, "\\rooms.txt");
+	ptr = fopen(adress, "r");
+	if (NULL == ptr) {
+		perror("Could not open rooms.txt");
+			return 1;
+	  }
+	else {
+		while (!feof(ptr)) {
+			char price[5] = { '\o' }, size[5] = { '\o' };
+			fscanf(ptr, "%s %s %s", (rooms + i)->name, price, size );
+			(rooms + i)->price= atoi(price);
+			(rooms + i)->size = atoi(size);
+			i++;
+			rooms = (Room*)realloc(rooms, sizeof(Room));
+			Room_default(rooms + i);
+		}
+	}
+	
+	
+	//return rooms;
 }
 
-static void consume_item(int item)
+static Guest* init_guests(char path) // create a thread for each guest
 {
-	/* dummy implementation, supplied just to make the program compile */
+	Guest *guests;
+
+	return guests;
+}
+static Hotel* init_hotel(Room *rooms, Guest *guest) // match designated room for each guest
+{
+	Hotel *hotel;
+
+	return hotel;
+	
 }
--- Ex3/receptionist.h
+++ Ex3/receptionist.h
@@ -5,6 +5,7 @@
 #include "windows.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 //structs ----------------------------------------------------------------------
 typedef struct {
@@ -16,8 +17,11 @@ typedef struct {
 }Room; 
 typedef struct {
 
-	char  *name; // name of the guest
+	char *name; // name of the guest
 	int money; // amount of monet guest has 
+	bool checked_in;
+	Room *designated_room;
+	int	day;
 	 
 
 }Guest;
@@ -32,8 +36,8 @@ typedef struct {
 
 // Constants  ------------------------------------------------------------------
 
-#define NUM_OF_THREADS 3
-#define MAX_STRING 100
+#define NUM_OF_THREADS 1
+#define MAX_STRING 500
 static const int TIMEOUT_IN_MILLISECONDS = 10 * 1000;
 static const int PRODUCER_WAIT_TIME_IN_MILISECONDS = 250;
 static const int CONSUMER_WAIT_TIME_IN_MILISECONDS = 500; /* Unequal wait times make the demonstration clearer */
@@ -49,12 +53,10 @@ static int items = 20;
 // Function Declarations -------------------------------------------------------
 
 static void reception();
-static DWORD Producer(LPVOID Dummy);
 static void ReportErrorAndEndProgram();
-static int produce_item();
-static void insert_item(int item);
-static int remove_item();
-static void consume_item(int item);
-
-
+static void check_in(Guest *guest);
+static void check_out(Guest *guest);
+static Hotel* init_hotel(char path);
+static void init_rooms(char path, Room *rooms);
+static Guest* init_guests(Room *rooms, Guest *guest);
 #endif //__RECEPTIONIST_H__
